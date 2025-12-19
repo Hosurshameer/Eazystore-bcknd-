feca4867a5096acb301cbf56b06f9eea44b66152
Hosurshameer <shameerhosur29@gmail.com>
2025-12-19 11:12:54 +0530

    Added dummy controller and the dummy dto to test url with @RequestBody,@RequestPram,@RequestHeader,@PathVariable

--- src/main/java/com/eazybytes/eazystore/controller/DummyController.java
+++ src/main/java/com/eazybytes/eazystore/controller/DummyController.java
@@ -51,6 +51,12 @@ public class DummyController {
         return "Searching for user : " + pathVariables.get("userId") + " and post : "
                 + pathVariables.get("postId");
     }
+//
+//    @GetMapping({"/user/map/{userId}/posts/{postId}", "/user/map/{userId}"})
+//    public String getUserUsingMap(@PathVariable Map<String,String> pathVariables) {
+//        return "Searching for user : " + pathVariables.get("userId") + " and post : "
+//                + pathVariables.get("postId");
+//    }
 
 
 }
