411b96c1ba7b936e1d2bb30cc799b495be9d8bd8
Hosurshameer <shameerhosur29@gmail.com>
2026-01-01 11:16:10 +0530

    added new RestAPi registerUser for registering purpose

--- src/main/java/com/eazybytes/eazystore/controller/AuthController.java
+++ src/main/java/com/eazybytes/eazystore/controller/AuthController.java
@@ -3,8 +3,10 @@ package com.eazybytes.eazystore.controller;
 
 import com.eazybytes.eazystore.dto.LoginRequestDto;
 import com.eazybytes.eazystore.dto.LoginResponseDto;
+import com.eazybytes.eazystore.dto.RegisterRequestDto;
 import com.eazybytes.eazystore.dto.UserDto;
 import com.eazybytes.eazystore.util.JwtUtil;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,17 +15,24 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
 private  final AuthenticationManager authenticationManager;
+private final InMemoryUserDetailsManager inMemoryUserDetailsManager;
+private final PasswordEncoder passwordEncoder;
 private  final JwtUtil jwtUtil;
 
     @PostMapping("/login")
@@ -34,6 +43,7 @@ private  final JwtUtil jwtUtil;
              var user=new UserDto();
              var loggedInUser=(User)authentication.getPrincipal();
            user.setName(loggedInUser.getUsername());
+
 
              String jwtToken=jwtUtil.generateJwtToken(authentication);
              return ResponseEntity.ok()
@@ -50,6 +60,21 @@ private  final JwtUtil jwtUtil;
          }
 
     }
+    @PostMapping("/register")
+    private  ResponseEntity<String> registerUser(@Valid @RequestBody RegisterRequestDto registerRequestDto){
+
+
+         inMemoryUserDetailsManager.
+                 createUser(new User(registerRequestDto.getEmail(),
+                         passwordEncoder.encode(registerRequestDto.getPassword()),
+                         List.of(new SimpleGrantedAuthority("USER"))));
+        return ResponseEntity.status(HttpStatus.CREATED).body("Registration Successful");
+
+    }
+
+
+
+
 
     private ResponseEntity<LoginResponseDto> buildErrorResponse(HttpStatus status,String message){
 
