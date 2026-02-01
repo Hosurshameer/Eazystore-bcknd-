bb938f95204cc11437fd10acd4a0edd3545125a7
Hosurshameer <shameerhosur29@gmail.com>
2026-02-01 09:28:35 +0530

    Added the roles in the jwtToken

--- src/main/java/com/eazybytes/eazystore/util/JwtUtil.java
+++ src/main/java/com/eazybytes/eazystore/util/JwtUtil.java
@@ -8,11 +8,13 @@ import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -27,6 +29,7 @@ public class JwtUtil {
                 .claim("username",fetchedCustomer.getName())
                 .claim("email",fetchedCustomer.getEmail())
                 .claim("mobileNumber",fetchedCustomer.getMobileNumber())
+                .claim("roles",authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(",")))
                 .issuedAt(new java.util.Date())
                 .expiration(new java.util.Date((new java.util.Date()).getTime()+60*60*1000))
                 .signWith(secretKey).compact();
