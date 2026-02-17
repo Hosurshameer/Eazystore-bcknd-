d3be2e4497f6068b2292722e7d2d530376245dde
Hosurshameer <shameerhosur29@gmail.com>
2026-02-17 10:08:39 +0530

    ....

--- src/main/java/com/eazybytes/eazystore/EazystoreApplication.java
+++ src/main/java/com/eazybytes/eazystore/EazystoreApplication.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 public class EazystoreApplication { 
     public static void main(String[] args) {
         SpringApplication.run(EazystoreApplication.class, args);
-
     }
 }
 
