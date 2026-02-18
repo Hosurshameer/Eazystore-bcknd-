9e1cbb02e58221a5286ede308c77879dd34a2475
Hosurshameer <shameerhosur29@gmail.com>
2026-02-18 11:26:37 +0530

    Added some jpql and native sql queries in the OrderRepository class

--- src/main/java/com/eazybytes/eazystore/repository/OrderRepository.java
+++ src/main/java/com/eazybytes/eazystore/repository/OrderRepository.java
@@ -13,9 +13,21 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByCustomerOrderByCreatedAtDesc(Customer customer );
 
     List<Order> findByOrderStatus(String orderStatus);
-
+//this is called jpql(java persistence query language)query which operates on the entity and filelds of entity
     @Query("select o from Order o where o.customer=:customer order by o.createdAt desc")
     List<Order> findOrderByCustomer(@Param("customer") Customer customer );
+
+
+//we can use ?1 as a placeholder for the first parameter. this is called positional parameter
+//    @Query("select o from Order o where o.customer=?1 order by o.createdAt desc")
+//    List<Order> findOrderByCustomer(@Param("customer") Customer customer );
+
+
+//    this is called native sql query which directly operates on the database
+//    @Query(value = "select * from orders o where o.customer_id=:customerId order by o.created_at desc",nativeQuery = true)
+//    List<Order> findOrderByCustomer(@Param("customerId") Long customerId);
+
+
 
 
 
