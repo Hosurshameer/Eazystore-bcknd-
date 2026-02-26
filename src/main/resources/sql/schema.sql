f603f4cf598c1be9a0e1d4b6f3d1d4770cbd2d2f
Hosurshameer <shameerhosur29@gmail.com>
2026-02-26 12:58:54 +0530

    updated the roles shchema for many to many relation with customer

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -48,13 +48,13 @@ CREATE TABLE IF NOT EXISTS address
 
 CREATE TABLE IF NOT EXISTS roles (
     role_id     BIGINT AUTO_INCREMENT PRIMARY KEY,
-    customer_id BIGINT NOT NULL,
+
     name        VARCHAR(50) NOT NULL,
     created_at TIMESTAMP   DEFAULT CURRENT_TIMESTAMP NOT NULL,
     created_by VARCHAR(20) NOT NULL,
     updated_at TIMESTAMP   DEFAULT NULL,
     updated_by VARCHAR(20) DEFAULT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON DELETE CASCADE
+    UNIQUE KEY unique_name(name);
     );
 
 
