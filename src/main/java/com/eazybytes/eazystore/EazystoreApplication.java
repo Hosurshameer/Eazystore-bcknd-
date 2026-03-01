a992f70655247be15903c77d936440ce9c53d1f7
Hosurshameer <shameerhosur29@gmail.com>
2026-03-01 15:21:53 +0530

    Added the @EnableCaching inside the EazystoreApplication.java class

--- src/main/java/com/eazybytes/eazystore/EazystoreApplication.java
+++ src/main/java/com/eazybytes/eazystore/EazystoreApplication.java
@@ -2,12 +2,14 @@ package com.eazybytes.eazystore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
+@EnableCaching
 public class EazystoreApplication { 
     public static void main(String[] args) {
         SpringApplication.run(EazystoreApplication.class, args);
