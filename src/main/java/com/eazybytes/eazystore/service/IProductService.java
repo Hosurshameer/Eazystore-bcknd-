646e4a54c123a34e2c6ba14942ebb6a90162e186
Hosurshameer <shameerhosur29@gmail.com>
2026-03-02 15:42:42 +0530

    Added the pagination logic in the service layer

--- src/main/java/com/eazybytes/eazystore/service/IProductService.java
+++ src/main/java/com/eazybytes/eazystore/service/IProductService.java
@@ -1,9 +1,12 @@
 package com.eazybytes.eazystore.service;
 
+import com.eazybytes.eazystore.dto.PageResponseDto;
 import com.eazybytes.eazystore.dto.ProductDto;
 import com.eazybytes.eazystore.entity.Product;
+import org.springframework.data.domain.Pageable;
+
 import java.util.List;
 
 public interface IProductService {
-    List<ProductDto> getProducts();
+    PageResponseDto getProducts(Pageable pageable);
 }
--- src/main/java/com/eazybytes/eazystore/service/impl/ProductServiceImpl.java
+++ src/main/java/com/eazybytes/eazystore/service/impl/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.eazybytes.eazystore.service.impl;
 
+import com.eazybytes.eazystore.dto.PageResponseDto;
 import com.eazybytes.eazystore.dto.ProductDto;
 import com.eazybytes.eazystore.entity.Product;
 import com.eazybytes.eazystore.repository.ProductRepository;
@@ -9,6 +10,8 @@ import com.fasterxml.jackson.databind.util.BeanUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,8 +25,11 @@ public class ProductServiceImpl implements IProductService {
 
     @Cacheable("products")
     @Override
-    public List<ProductDto> getProducts() {
-        return productRepository.findAll().stream().map(this::transformToDto).collect(Collectors.toList());
+    public PageResponseDto getProducts(Pageable pageable) {
+        Page<Product> page=productRepository.findAll(pageable);
+       List<ProductDto>content=page.getContent().stream().map(this::transformToDto).collect(Collectors.toUnmodifiableList());
+
+       return new PageResponseDto(content,page.getNumber(),page.getTotalPages(), page.getTotalElements(),page.getSize());
     }
     private ProductDto transformToDto(Product product){
         ProductDto productDto=new ProductDto();
